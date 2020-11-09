6ef219ad2ccfc232d32a369ae8a42dc6dfbd08f0
DESKTOP-TLT9OCT\kyumi <kyumin12271227@gmail.com>
2020-11-09 23:04:51 +0900

    1110번 변경 전

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,27 @@
-import java.io.*;
+import java.util.Scanner;
 public class Main {
-
 	public static void main(String[] args) {
 		
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		Scanner input = new Scanner(System.in);
 		
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int num1, num2, num3, num4, num5, count = 0;
 		
-		int N, A, B;
-		
-		String S = bf.readLine();
-		
-		int i = Integer.parseInt(bf.readLine());
-		
-		for (int j = 0; j < N; j++) {
-			
-		}
+		num1 = input.nextInt();
+		num3 = num1 % 10;	
+		num2 = (num1 - num3) / 10;
+		while(true) {
+			count += 1;
+			num4 = num2 + num3;
+			if (num4 < 10) {
+				num2 = num3 * 10;
+				num3 = num4;
+			} else {
+				num2 = num3 * 10;
+				num3 = num4 % 10;
+			} num5 = num2 + num3;
+			if (num5 == num1) {
+				break;
+			}
+		} System.out.println(count);
 	}
-
 }
