ea6f1947a3e691d5a0fa298c8acea416d755a033
DESKTOP-TLT9OCT\kyumi <kyumin12271227@gmail.com>
2020-11-09 23:12:55 +0900

    1110 문제 성공!

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,15 @@ public class Main {
 		int num1, num2, num3, num4, num5, count = 0;
 		
 		num1 = input.nextInt();
-		num3 = num1 % 10;	
-		num2 = (num1 - num3) / 10;
+		num5 = num1;
 		while(true) {
 			count += 1;
+			num3 = num5 % 10;	
+			num2 = (num5 - num3) / 10;
 			num4 = num2 + num3;
-			if (num4 < 10) {
-				num2 = num3 * 10;
-				num3 = num4;
-			} else {
-				num2 = num3 * 10;
-				num3 = num4 % 10;
-			} num5 = num2 + num3;
+			num2 = num3 * 10;
+			num3 = num4 % 10;
+			num5 = num2 + num3;
 			if (num5 == num1) {
 				break;
 			}
