eda9e3ad0c69bc0c2672fa939524617ad5ee416a
DESKTOP-TLT9OCT\kyumi <kyumin12271227@gmail.com>
2020-11-15 00:07:00 +0900

    3052번 문제

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,38 @@ public class Main {
 		
 		Scanner input = new Scanner(System.in);
 		
-		int [] array = new int [10];
-		int A, B, C, num, num1 = 10, num2 = 0, num3 = 0;
+		int[] array = new int [10];
+		int count = 0, count2 = 0, count0 = 0, num;
 		
-		A = input.nextInt();	B = input.nextInt(); 	C = input.nextInt();
-		
-		num = A * B * C;
-		
-		for (int i = 0; i <= (int)Math.log10(num); i++) {
-			num2 = (num % num1 - num3) / (num1 / 10);
-			for (int ii = 0; ii < array.length; ii++) {
-				if (num2 == ii) {
-					array[ii] += 1;	break;
-				}
-			} num3 += (num2 * num1) / 10;	num1 *= 10;
-		} for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
+		//수 받기
+		for(int i = 0; i < array.length; i++) {
+			array[i] = input.nextInt();
 		}
+		
+		int[] array2 = new int[array.length];
+		
+		//배열1의 i번째 값을 42로 나누어서 배열2에 저장된 값과 비교하여 같으면 count2 를 증가시킴 
+		for(int i = 0; i < array.length; i++) {
+			num = array[i] % 42;
+			for(int ii = 0; ii < array2.length; ii++) {
+				if(num == array2[ii]) {
+					count2 += 1;
+				}
+			} 
+			//count2가 0일 경우 겹치지 않는다고 판단하여 count를 증가시키고 배열2에 해당 값을 추가
+			if(count2 == 0) {
+				array2[count] = num;
+				count+=1;
+			}
+			//0이 처음 나올 경우 count 1 증가
+			if(num == 0 && count0 == 0) {
+				count+=1;
+				count0+=1;
+			}
+			count2 = 0;
+		}
+		
+		System.out.println(count);
 		
 	}
 
