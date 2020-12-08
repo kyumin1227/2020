f51cb5b63c488ceda362fd5421ec6192e6e1470e
DESKTOP-TLT9OCT\kyumi <kyumin12271227@gmail.com>
2020-12-08 21:11:32 +0900

    백준 4504 실패

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,22 @@ public class Main {
 		
 		Scanner input = new Scanner(System.in);
 		
-		int num;
+		int n, num, count = 0;
 		
-		num = input.nextInt();
+		n = input.nextInt();
 		
-		int array [] = new int [num + 1];
-		
-		array[0] = 0;
-		array[1] = 1;
-		
-		for(int i = 2; i <= num; i++) {
-			array[i] = array[i - 1] + array[i - 2];
+		for(int i = 0; i < 10; i++) {
+			num = input.nextInt();
+			for(int j = 1; j <= 10000; j++) {
+				if (n * j == num) {
+					System.out.println(num + " is a multiple of " + n + ".");
+					count += 1;
+				}
+			} if (num == 0) {
+				
+			} else if (count == 0) {
+				System.out.println(num + " is NOT a multiple of " + n + ".");
+			} count = 0;
 		}
-		System.out.println(array[num]);
 	}
 }
