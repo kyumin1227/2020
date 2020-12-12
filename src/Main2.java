d8fdd3bac1bb1bf41abce2d92bbc2506dca76516
DESKTOP-TLT9OCT\kyumi <kyumin12271227@gmail.com>
2020-12-12 19:46:22 +0900

    백준 17609 시간 초과

--- src/Main2.java
+++ src/Main2.java
@@ -1,4 +1,60 @@
-
+import java.util.Scanner;
 public class Main2 {
+	public static void main (String [] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int T;
+		
+		T = input.nextInt();
+		
+		String [] array = new String [T];
+		int [] array3 = new int [T];
+		
+		for (int i = 0; i < T; i++) {
+			array[i] = input.next();
+			char [] array2 = new char [array[i].length()];
+			for (int j = 0; j < array[i].length(); j++) {
+				array2[j] = array[i].charAt(j);
+			} for (int j = 0; j < array[i].length() / 2; j++) {
+				if (array2[j] == array2[array[i].length() - 1 - j]) {
 
+				} else {
+					array3[i] = 1;
+				}
+			}
+			int num1 = 0, num2 = 0, count2 = 0;
+			if (array3[i] == 1) {
+				for (int j = 0; j < array[i].length(); j++) {
+					array2[j] = array[i].charAt(j);
+				} for (int j = 0; j < array[i].length(); j++) {
+					int count = 0;
+					num1 = 0;
+					num2 = 0;
+					for (int ii = 0; ii < array[i].length() / 2; ii++) {
+						if (ii == j && ii < array[i].length() / 2) {
+							num1 = 1;
+							num2 = 0;
+						} else if (ii == array[i].length() - j - 1 && j > array[i].length() / 2) {
+							num1 = 0;
+							num2 = 1;
+						} if (array2[ii + num1] == array2[array[i].length() - ii - 1 - num2]) {
+							
+						} else {
+							count += 1;
+						}
+					}
+					if (count < 1) {
+						count2 += 1;
+					}
+				}
+				if (count2 >= 1) {
+					array3[i] = 1;
+				} else {
+					array3[i] = 2;
+				}
+			}
+			System.out.println(array3[i]);
+		}
+	}
 }
